2bcc2d817249e318baf09bb3e0d9b1ca1f3ae98b
DongUk Jeon <machska@gmail.com>
2014-06-09 16:01:57 +0800

    Add push notification test view controller

---
+++ StudyTopic-iOS/PushTest/PushViewController.m
@@ -0,0 +1,38 @@
+//
+//  PushViewController.m
+//  StudyTopic-iOS
+//
+//  Created by DongUk Jeon on 2014. 6. 9..
+//  Copyright (c) 2014년 HuchCode. All rights reserved.
+//
+
+#import "PushViewController.h"
+
+@interface PushViewController ()
+
+@end
+
+@implementation PushViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
