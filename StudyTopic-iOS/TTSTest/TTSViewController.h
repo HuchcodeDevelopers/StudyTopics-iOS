c29973afdb0c58d84c804eac5019857096cb3d69
DongUk Jeon <machska@gmail.com>
2014-06-09 16:22:01 +0800

    Add TTS test view controller

---
+++ StudyTopic-iOS/TTSTest/TTSViewController.h
@@ -0,0 +1,13 @@
+//
+//  TTSViewController.h
+//  StudyTopic-iOS
+//
+//  Created by DongUk Jeon on 2014. 6. 9..
+//  Copyright (c) 2014년 HuchCode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TTSViewController : UIViewController
+
+@end
---
+++ StudyTopic-iOS/TTSTest/TTSViewController.m
@@ -0,0 +1,38 @@
+//
+//  TTSViewController.m
+//  StudyTopic-iOS
+//
+//  Created by DongUk Jeon on 2014. 6. 9..
+//  Copyright (c) 2014년 HuchCode. All rights reserved.
+//
+
+#import "TTSViewController.h"
+
+@interface TTSViewController ()
+
+@end
+
+@implementation TTSViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
